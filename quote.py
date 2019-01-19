50ba962a27100364ff165cca362026ba2c95f513
Ha Truong <truonghatsts@gmail.com>
2019-01-19 21:13:20 +0700

    Rename folder to sounds to static and add flask

--- quote.py
+++ quote.py
@@ -3,6 +3,7 @@ import os
 import time
 import random
 import codecs
+from flask import Flask
 
 def load_quotes(filename):	
 	text_file = codecs.open(filename, encoding='utf-8')
@@ -10,31 +11,35 @@ def load_quotes(filename):
 	lines = [line.strip() for line in lines]
 	return lines
 
-def check_exists(i, sound_path='./sounds/'): 
+def check_exists(i, sound_path='./static/'): 
 	filename = sound_path + str(i) + '.mp3'
 	return os.path.isfile(filename)
 
-def create_missing_sounds(quotes, sound_path='./sounds/'):
+def create_missing_sounds(quotes, sound_path='./static/'):
 	for i in range(len(quotes)):		
 		if not check_exists(i):
 			text = quotes[i]	
 			tts = gTTS(text=text, lang='en')
 			tts.save(sound_path + str(i) + ".mp3")
 
-def ring(quotes, duration, sound_path='./sounds/'):
-	beep = sound_path + 'beep.mp3'
-	while True:
-		i = random.randint(0, len(quotes) - 1)
-		print(quotes[i])		
-		os.system('start ' + beep)		
-		time.sleep(3)
-		os.system('start ' + sound_path + str(i) + '.mp3')
-		time.sleep(duration)
+interval = 30
+quotes = load_quotes('data.txt')
+create_missing_sounds(quotes)
+app = Flask(__name__)	
 
-def main():
-	quotes = load_quotes('data.txt')
-	create_missing_sounds(quotes)
-	ring(quotes, 60)	
+@app.route("/")
+def home():
 
-if __name__== "__main__":
-	main()
+	quote_index = random.randint(0, len(quotes) - 1)
+	quote = quotes[quote_index]	
+	audio = "<audio controls autoplay><source src='/static/" + str(quote_index) + ".mp3' type='audio/mpeg'></audio>"
+	text = (
+		'<html>'
+		+ '<head>'
+		+ '<meta http-equiv="refresh" content="' + str(interval) + '">'
+		+ '</head><body style="font-size: 3em; text-align: center; background-color: green; color: white">'
+		+ quote + "<br/>"
+		+ audio
+		+ "</body></html>"
+	)
+	return text
